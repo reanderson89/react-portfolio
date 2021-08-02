f2f7138cf2077d513f1d37cc95e83b9caf23992a
Robert Anderson <reanderson89@gmail.com>
2021-08-02 08:52:07 -0700

    updated about me page content

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,18 +8,9 @@ import Box from '@material-ui/core/Box';
 
 
 
-const aboutMe = `Recent graduate from Georgia Tech's Full Stack Web Development Program. I have worked remotely with different groups of people to develop 3 applications using agile methodology. One of them was focused on the use of API's and the other 2 were focused on being full stack applications that ended with the final application being MERN stack. Front end is really where I have found enjoyment in coding, specifically using React. Throughout the program I have enjoyed the struggle of learning something new and also being able to help others succeed. Almost 9 years of Coaching Crossfit has helped to hone in my communication and leadership skills and has allowed me to do the thing I really enjoy most, which is helping others realize that they are capable of more then they believe.`
+const aboutMe = `My name is Robert Anderson, I have worked remotely with different groups of people to develop 3 applications using agile methodology. "Search-U" uses multilpe API's to create a search function to find quality of life scores near colleges. The other 2 are full stack applications, one being "BarkMatch" that helps dog shelters find dog walking volunteers and "Farm to Table" that creates an e-commerce for local farmers to have easier access to sales in their community.`
 
-const aboutMeCont = `Outside of programming I find that
-the things I enjoy most are very simple. I like to spend time with
-my wife who is a bada$$ nurse. We have two "classy ladies" (senior
-dogs) who we are very grateful have kept happy and healthy through
-the years. We will go and hike and be adventurous but we will also
-spend hours at home cooking and watching netflix. Though I would
-call myself a homebody I do like to go and hangout with friends
-either at someones house, a restaurant, brewery, park, maybe even
-playing some tennis or golf (sports I suck at, but enjoy sucking
-at).`
+const aboutMeCont = `Front-end development is really where I have found enjoyment in coding, specifically using React. Throughout the program I have enjoyed the struggle of learning something new and also being able to help others succeed. Almost 9 years of Coaching Crossfit has helped to hone in my communication and leadership skills and has allowed me to do the thing I really enjoy most, which is helping others realize that they are capable of more then they believe.`
 
 const useStyles = makeStyles((theme) => ({
     root: {
